8cdaff4bf0de4716e530a1c5a3803ab606ee8d4d
rami216 <chamseddinerami@gmail.com>
2025-08-28 20:27:48 +0300

    removed Removed the trailing slash

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ app = FastAPI()
 PUBLIC_RULES = [
     ("/site-auth", {"GET", "POST", "OPTIONS"}),                 # <- allow both '/site-auth' and '/site-auth/...'
     ("/users-stripe-account/public/", {"POST", "OPTIONS"}),  # checkout
-    ("/users-stripe-account/public/stripe-key/", {"GET", "OPTIONS"}),
+    ("/users-stripe-account/public/stripe-key", {"GET", "OPTIONS"}),
     ("/checkout/", {"POST", "OPTIONS"}),
     ("/public/", {"GET", "OPTIONS"}),
     ("/locations/", {"GET", "OPTIONS"}),
